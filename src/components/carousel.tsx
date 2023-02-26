6a2740098c28cf39f5c10c6d3aa14237817fbcc6
Esteban <esteban.villegas92@hotmail.com>
2023-02-26 18:31:37 -0500

    Finished carousel component

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,14 +1,65 @@
-import { useRef } from "react";
+import React from "react";
+import { useRef, useEffect, useState } from "react";
 import styles from "./carousel.module.css";
+
+const carCardWidth: number = 290;
+
+const determineDotIsActive = (
+  scrollPos: number,
+  elementIndex: number
+): boolean => {
+  const currentElemAmount: number = carCardWidth * elementIndex;
+  const nextElemAmount: number = carCardWidth * (elementIndex + 1);
+  if (scrollPos > currentElemAmount && scrollPos <= nextElemAmount) {
+    return true;
+  }
+  return false;
+};
 
 export default function Carousel({ children }: { children: React.ReactNode }) {
   const carouselRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const numElems: number = React.Children.count(children);
+
+  const handleScroll = () => {
+    const scrollLeft: number | undefined = carouselRef.current?.scrollLeft;
+    setScrollPosition(scrollLeft ? scrollLeft : 0);
+  };
+
+  useEffect(() => {
+    carouselRef.current?.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+
+    return () => {
+      carouselRef.current?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function renderDots() {
+    const result = [];
+    for (let i = 0; i < numElems; i++) {
+      result.push(
+        <span
+          className={`${styles.scrollDot} ${
+            determineDotIsActive(scrollPosition, i)
+              ? styles.scrollDotActive
+              : ""
+          }`}
+          key={i}
+        ></span>
+      );
+    }
+    return result;
+  }
+
   function scrollRight(): void {
     carouselRef.current?.scrollBy(200, 0);
   }
   function scrollLeft(): void {
     carouselRef.current?.scrollBy(-200, 0);
   }
+
   return (
     <div className={styles.carouselCover}>
       <div ref={carouselRef} className={styles.carouseContainer}>
@@ -22,7 +73,7 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
             r="18.5"
             fill="white"
             stroke="#333"
-            stroke-width="1"
+            strokeWidth="1"
           ></circle>
           <rect
             x="24.9141"
@@ -33,7 +84,7 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
             transform="rotate(-135 24.9141 20.3457)"
             fill="none"
             stroke="#333"
-            stroke-width="1"
+            strokeWidth="1"
           ></rect>
           <rect
             x="17.4942"
@@ -44,7 +95,7 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
             transform="rotate(-45 17.4942 27.0703)"
             fill="none"
             stroke="#333"
-            stroke-width="1"
+            strokeWidth="1"
           ></rect>
         </svg>
       </span>
@@ -56,7 +107,7 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
             r="18.5"
             fill="white"
             stroke="#333"
-            stroke-width="1"
+            strokeWidth="1"
           ></circle>
           <rect
             x="24.9141"
@@ -67,7 +118,7 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
             transform="rotate(-135 24.9141 20.3457)"
             fill="none"
             stroke="#333"
-            stroke-width="1"
+            strokeWidth="1"
           ></rect>
           <rect
             x="17.4942"
@@ -78,10 +129,11 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
             transform="rotate(-45 17.4942 27.0703)"
             fill="none"
             stroke="#333"
-            stroke-width="1"
+            strokeWidth="1"
           ></rect>
         </svg>
       </span>
+      <div className={styles.scrollDots}>{renderDots()}</div>
     </div>
   );
 }
